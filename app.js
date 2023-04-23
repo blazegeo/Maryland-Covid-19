1c0f4d78456f7eaeb42136624cdfb67980f84fe2
Abdul Samateh <60404129+blazegeo@users.noreply.github.com>
2023-04-23 09:57:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function initializeViz() {
 var placeholderDiv = document.getElementById("tableauViz");
-var url = "https://public.tableau.com/views/MDCovid-19/Story1?:language=en-US&:display_count=n&:origin=viz_share_link";
+var url = "https://public.tableau.com/shared/MGZGXT46H?:display_count=n&:origin=viz_share_link";
 var options = {
  width: '600px',
  height: '600px',
