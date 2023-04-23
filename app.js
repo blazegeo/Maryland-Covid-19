8bc3e471acd09b3a84eb7eb825badb90992493d3
Abdul Samateh <60404129+blazegeo@users.noreply.github.com>
2023-04-23 09:32:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/shared/MGZGXT46H?:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/MDCovid-19/Story1?:language=en-US&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
